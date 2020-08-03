8c9bba09176e9f2186918a5f975824a1545747ae
noriyuki.kurokawa <badphantom@gmail.com>
2020-08-03 21:10:50 +0900

    不要コメントを削除

--- Startup.cs
+++ Startup.cs
@@ -22,30 +22,22 @@ namespace Uls.Shigemaru
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
-            // Create the bot services(QnA) as a singleton.
             services.AddSingleton<IBotServices, BotServices>();
 
-            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
             services.AddSingleton<IStorage, MemoryStorage>();
 
-            // Create the User state. (Used in this bot's Dialog implementation.)
             services.AddSingleton<UserState>();
 
-            // Create the Conversation state. (Used by the Dialog system itself.)
             services.AddSingleton<ConversationState>();
 
-            // The Dialog that will be run by the bot.
             services.AddSingleton<RootDialog>();
 
-            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, QnABot<RootDialog>>();
 
             string storageAccount = Configuration.GetValue<string>("StorageAccount");
@@ -53,13 +45,10 @@ namespace Uls.Shigemaru
             AzureBlobStorage storage = new AzureBlobStorage(storageAccount, storageContainerName);
             services.AddSingleton<IStorage>(storage);
 
-            // Create the Conversation state. (Used by the Dialog system itself.)
             services.AddSingleton<ConversationState>();
 
-            // Create a global hashset for our ConversationReferences
             services.AddSingleton<ConcurrentDictionary<string, ConversationReference>>();
 
-            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddBot<QnABot<RootDialog>>(options => {
                 options.Middleware.Add(new ContextLogger());
 
@@ -68,7 +57,6 @@ namespace Uls.Shigemaru
             services.AddSingleton<NotificationDialog>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
